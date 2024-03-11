74b2496c8bae1cbf7243c8cda832597f59e53eef
Jenn Bourke <jennfbourke@gmail.com>
2024-03-11 16:17:45 -0400

    Updated Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,6 @@ function Nav() {
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/countries'>Countries</NavLink>
             <NavLink to='/add-country'>Add Country</NavLink>
-            <NavLink to='/edit-country'>Edit Country</NavLink>
         </nav>
     )
 };
